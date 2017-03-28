23acc73bf11cc55be67838d4ef61d3455470bd78
gabjetté <gabjet.web@gmail.com>
2017-03-28 17:06:13 -0400

    Pas mal de modifications...en fait l'exercice est fini

--- serveur.js
+++ serveur.js
@@ -1,40 +1,50 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var obj;
+
+
+function affiche_objet(obj) {
+	var ch='<table>'
+	for (i=0 ; i<obj.length; i++){
+		var objet = obj[i];
+		for (propriete in objet) {
+			ch+="<tr>"+"<td>"+propriete+'</td>'+'<td>'+objet[propriete]+'</td>'+"</tr>";
+		}
+	}
+ch += '</table>'
+return ch;
+}
 
 
 // Créer un serveur
-http.createServer( function (req, res) {  
+http.createServer( function (request, response) {  
    // On extrait de la requête «request» le chemin  qui nous donnera le nom de fichier
    var pathname = url.parse(request.url).pathname;
-   
- 
-   console.log("request.url = " + request.url)
-   console.log("url.parse(request.url).pathname = " + url.parse(request.url).pathname)
   // affiche le nom du fichier pour laquelle la requête a été généré
    console.log("Request for " + pathname + " received.");
-   
 
-   // Lire par le «fs» (file système) le fichier de la requête 
-   // le slice(1) permet de retirer le premier caractère
-   fs.readFile("provinces.txt", function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else { 
-         //Page found   
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});  
-         
-         // affiche le contenu du fichier dans la page HTML
-         response.write(data.toString());   
-      }
-      // transmet la reponse  
-      response.end();
-   });   
+	if(request.url=='/provinces') {
+		fs.readFile('provinces.json', 'utf-8', function (err, data) {
+		if (err) return console.error(err);
+		obj = JSON.parse(data)
+
+		  response.writeHead(200, {"Content-Type": "text/html"});
+		  response.write(affiche_objet(obj));
+		  response.end();
+		});
+
+	} else {
+		fs.readFile('etats.json', 'utf-8', function (err, data) {
+		if (err) return console.error(err);
+		obj = JSON.parse(data)
+		console.log(obj);
+		response.writeHead(200, {"Content-Type": "text/html"});
+	  response.write(affiche_objet(obj));
+ 	 response.end();
+		});
+	}
+
 }).listen(8081);
 
 // message console
